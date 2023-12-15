0f83074c03a04517b6f8f42167c058517c478e27
MiguelMC <mcmiguel313@gmail.com>
2023-12-15 23:04:57 +0100

    Examen

--- parcial3_fe/src/app/services/prueba-serv/prueba.service.ts
+++ parcial3_fe/src/app/services/prueba-serv/prueba.service.ts
@@ -10,18 +10,18 @@ export class PruebaService {
   constructor(private http:HttpClient) { }
 
   getAll() : Observable<any> {
-    const url = 'http://localhost:8000/api/eventos/';
-    //const url = 'http://13.36.188.166:8000/api/pruebas/';
+    //const url = 'http://localhost:8000/api/eventos/';
+    const url = 'http://13.36.188.166:8000/api/eventos/';
     return this.http.get<any>(url);
   }
   getPostal(postal: string) : Observable<any> {
-    const url = 'http://localhost:8000/api/eventoPostal/'+postal+'/';
-    //const url = 'http://13.36.188.166:8000/api/pruebas/';
+    //const url = 'http://localhost:8000/api/eventoPostal/'+postal+'/';
+    const url = 'http://13.36.188.166:8000/api/eventoPostal/'+postal+'/';;
     return this.http.get<any>(url);
   }
   getInfo(id: string) : Observable<any> {
-    const url = 'http://localhost:8000/api/eventos/'+id+'/';
-    //const url = 'http://13.36.188.166:8000/api/pruebas/';
+    //const url = 'http://localhost:8000/api/eventos/'+id+'/';
+    const url = 'http://13.36.188.166:8000/api/eventos/'+id+'/';
     return this.http.get<any>(url);
   }
 
